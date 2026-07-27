b282e24d7777c22edfcb0b1e4ac137ec0136e799
Michael <delamarre.michael.pro@gmail.com>
2026-07-27 12:08:30 +0200

    EXCEPTIONS: AJout des annotations pour GlobalExceptionHandler handleValidation avec une prévision BAD_REQUEST et JSON.

--- src/main/java/com/project/chatop/common/web/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/project/chatop/common/web/exceptions/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package com.project.chatop.common.web.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(exception = MethodArgumentNotValidException.class, produces = MediaType.APPLICATION_JSON_VALUE)
     public ProblemDetail handleValidation(MethodArgumentNotValidException ex) {
         ProblemDetail pd = ProblemDetail.forStatusAndDetail(
                 HttpStatus.BAD_REQUEST, "Requête invalide");
