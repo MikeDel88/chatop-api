7142710bcab6bc179af9a828fb9789a10dfe5fca
Michael <delamarre.michael.pro@gmail.com>
2026-07-27 18:14:38 +0200

    REFACTORING: Pour le PictureService, on modifie le nom de l'image avec un uuid pour avoir quelque chose d'unique mais lisible.

--- src/main/java/com/project/chatop/features/rentals/application/services/PictureService.java
+++ src/main/java/com/project/chatop/features/rentals/application/services/PictureService.java
@@ -1,14 +1,18 @@
 package com.project.chatop.features.rentals.application.services;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.UUID;
 
 @Service
 public class PictureService {
@@ -22,6 +26,12 @@ public class PictureService {
             throw new IOException("Failed to upload file");
         }
 
+        String contentType = file.getContentType();
+        assert contentType != null;
+        if (!contentType.equals("image/jpeg") && !contentType.equals("image/png")) {
+            throw new IllegalArgumentException("Only JPEG or PNG images are allowed");
+        }
+
         Path uploadPath = Paths.get(uploadDir);
         if (!Files.exists(uploadPath)) {
             Files.createDirectories(uploadPath);
@@ -31,9 +41,20 @@ public class PictureService {
         if(fileName == null) {
             throw new IOException("Failed to upload file");
         }
+
+        int dotIndex = fileName.lastIndexOf('.');
+        String extension = (dotIndex > 0) ? fileName.substring(dotIndex + 1) : "";
+
+        UUID uuid = UUID.randomUUID();
+        fileName = "rental_" + uuid + "." + extension;
         Path filePath = uploadPath.resolve(fileName);
         Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
 
-        return filePath.toString();
+        return fileName;
+    }
+
+    public Resource getImage(String filename) throws MalformedURLException {
+        Path filePath = Paths.get(uploadDir).resolve(filename);
+        return new UrlResource(filePath.toUri());
     }
 }
