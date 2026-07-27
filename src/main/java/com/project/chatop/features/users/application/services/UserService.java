8e1a90b471004b706ccb23fc44157ec5a839bb95
Michael <delamarre.michael.pro@gmail.com>
2026-07-27 15:17:51 +0200

    REFACTORING: Ajout du UserNotFoundException si on ne trouve pas l'utilisateur.

--- src/main/java/com/project/chatop/features/users/application/services/UserService.java
+++ src/main/java/com/project/chatop/features/users/application/services/UserService.java
@@ -4,6 +4,7 @@ import com.project.chatop.features.users.application.mappers.UserMapper;
 import com.project.chatop.features.users.domain.entities.User;
 import com.project.chatop.features.users.domain.repositories.UserRepository;
 import com.project.chatop.features.users.web.dtos.UserResponse;
+import com.project.chatop.features.users.web.exceptions.UserNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,6 +20,9 @@ public class UserService {
 
     public UserResponse getUser(Long userId) {
         User user = this.userRepository.findUserById(userId);
+        if (user == null) {
+            throw new UserNotFoundException();
+        }
         return this.userMapper.toUserResponse(user);
     }
 }
