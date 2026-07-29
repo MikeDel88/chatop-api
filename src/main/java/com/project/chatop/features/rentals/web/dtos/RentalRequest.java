8c5b5e7fdc96f2ed2bb6f93d79e7db35dfbe5e50
Michael <delamarre.michael@gmail.com>
2026-07-29 14:51:33 +0200

    fix(rental): le RentalRequest doit avoir des contraintes de validation.

--- src/main/java/com/project/chatop/features/rentals/web/dtos/RentalRequest.java
+++ src/main/java/com/project/chatop/features/rentals/web/dtos/RentalRequest.java
@@ -1,11 +1,14 @@
 package com.project.chatop.features.rentals.web.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.web.multipart.MultipartFile;
 
 public record RentalRequest(
-        String name,
-        Integer surface,
-        Integer price,
-        MultipartFile picture,
-        String description
+        @NotBlank String name,
+        @NotNull @Positive Integer surface,
+        @NotNull @Positive Integer price,
+        @NotNull MultipartFile picture,
+        @NotBlank String description
 ) {}
