a0631eed477dc4efeb8410a3acbd745df36b44b4
Michael <delamarre.michael@gmail.com>
2026-08-07 17:37:00 +0200

    refactor: utilisation de org.mapstruct pour générer les mappers de l'application.

--- src/main/java/com/project/chatop/mapper/RentalMapper.java
+++ src/main/java/com/project/chatop/mapper/RentalMapper.java
@@ -1,66 +1,31 @@
 package com.project.chatop.mapper;
 
-import com.project.chatop.entity.Rental;
 import com.project.chatop.dto.request.RentalRequest;
 import com.project.chatop.dto.response.RentalResponse;
+import com.project.chatop.entity.Rental;
 import com.project.chatop.entity.User;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Component;
-import java.time.format.DateTimeFormatter;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
-@Log4j2
-@Component
-public class RentalMapper {
+@Mapper(componentModel = "spring")
+public interface RentalMapper {
 
-    public Rental toCreateRental(RentalRequest rentalRequest, String pictureUrl, User owner) {
-        log.info("toCreateRental : {}", rentalRequest);
-        log.debug("toCreateRental : {}", pictureUrl);
-        log.debug("toCreateRental : {}", owner);
+    @Mapping(target = "name", source = "rentalRequest.name")
+    @Mapping(target = "picture", source = "pictureUrl")
+    @Mapping(target = "owner", source = "owner")
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    Rental toCreateRental(RentalRequest rentalRequest, String pictureUrl, User owner);
 
-        Rental rental = new Rental();
-        rental.setOwner(owner);
-        rental.setPrice(rentalRequest.price());
-        rental.setSurface(rentalRequest.surface());
-        rental.setPicture(pictureUrl);
-        rental.setName(rentalRequest.name());
-        rental.setDescription(rentalRequest.description());
-        log.debug("toCreateRental : {}", rental);
+    @Mapping(target = "picture", ignore = true)
+    @Mapping(target = "owner", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    Rental toUpdateRental(@MappingTarget Rental rental, RentalRequest rentalRequest);
 
-        return rental;
-    }
-
-
-    public Rental toUpdateRental(Rental rental, RentalRequest rentalRequest) {
-        log.info("toUpdateRental : {}", rentalRequest);
-        log.debug("toUpdateRental : {}", rental);
-
-        rental.setPrice(rentalRequest.price());
-        rental.setSurface(rentalRequest.surface());
-        rental.setName(rentalRequest.name());
-        rental.setDescription(rentalRequest.description());
-
-        log.debug("toUpdateRental : {}", rental);
-        return rental;
-    }
-
-    public RentalResponse toRentalResponse(Rental rental) {
-        log.info("toRentalResponse : {}", rental);
-
-        String europeanDatePattern = "yyyy/MM/dd";
-        DateTimeFormatter europeanDateFormatter = DateTimeFormatter.ofPattern(europeanDatePattern);
-
-        RentalResponse rentalResponse = new RentalResponse(
-                rental.getId(),
-                rental.getName(),
-                rental.getSurface(),
-                rental.getPrice(),
-                rental.getPicture(),
-                rental.getDescription(),
-                rental.getOwner().getId(),
-                europeanDateFormatter.format(rental.getCreatedAt()),
-                europeanDateFormatter.format(rental.getUpdatedAt())
-        );
-        log.debug("toRentalResponse : {}", rentalResponse);
-        return rentalResponse;
-    }
+    @Mapping(target = "owner_id", source = "owner.id")
+    @Mapping(target = "created_at", source = "createdAt", dateFormat = "yyyy/MM/dd")
+    @Mapping(target = "updated_at", source = "updatedAt", dateFormat = "yyyy/MM/dd")
+    RentalResponse toRentalResponse(Rental rental);
 }
